3241d3872e356c42a019b8d5039b5c2fc6986f56
aayush <kasaudhanaayush@gmail.com>
2022-04-28 02:54:28 +0530

    taskbox ui

---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log('Hello Project.');
+import { hello } from "./index1";
+
+hello()
+
---
+++ src/index1.js
@@ -0,0 +1,2 @@
+console.log('hello index1')
+export const hello =()=>document.getElementById("p1").innerHTML = "Text modified in webpack";
